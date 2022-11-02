ed1c6ed6beeb0d83767867a1bf4999ad9e93a6da
Conor Donnelly <Conor.Donnelly@state.mn.us>
2022-11-01 23:23:32 -0500

    adding final changes to the project after recording demo

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# 09-NodeJS-README
-Command line utility built with NodeJS to automatically build a professional looking README.md file for my projects.
+## Title Using Node to Generate README.md    
+
+## Description - Helps you quickly generat a README.md file.
+    
+- Motivation: Learning how to use node.JS and inquirer.
+- Problem solved: Quickly gererates a hiqh quality README file for your projects. 
+- Lessons learned: Learned how to use Node, Inquirer and formatting with template literals.
+    
+## Table of Contents    
+   
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Credits](#credits)
+
+- [License](#license)
+
+- [Screenshot](#screenshot)
+    
+## Installation    
+
+- Steps required to install project:
+1) install it 2) run it using node runtime
+    
+## Usage
+
+- Here are usage instructions:
+    Use it wisely or else!
+    
+## Screenshot
+
+![screenshot of app](/assets/images/screenshot.png)
+
+## Click Link for Screen Recording
+- <https://drive.google.com/file/d/1keAlKBWMuWQykePl-6k2mStmySCs3VvA/view>
+            
+    
+## Credits / Contact Info 
+
+- email me: <riversidempls@gmail.com>
+- Find me on GitHub at <https://github.com/riversidempls>
+
+    
+    
+## License Info
+
+Apache license 2.0,MIT,GNU General Public License v3.0
+
+## Badges 🏆
+
+Javascript,Node.JS   
+    
+## How to Contribute
+    
+-If you'd like to become a contributor, here's how you can help:
+If you want to help out, email me! OR contact via GitHub
+    
+## Tests
+  
+- How to run tests of the application:
+Try and break it    
--- index.js
+++ index.js
@@ -1,26 +1,236 @@
 // TODO: Include packages needed for this application
-inquirer = require('./node_modules/inquirer');
-const fs = require('fs');
+const inquirer = require('inquirer');
+var fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the title of your project?",
-    "What is your email address?",
-    "What is your GitHub username?",
-    "Type a project description:",
-    "Provide some installation instructions for users:",
-    "Enter usage information:",
-    "Provide some contribution instructions:",
-    "Provide some testing instructions:",
-    "Which license does this project operate under? (choose one)",
-    "Which, if any, of these technologies does your project utilize?"
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project?",
+    },
+    {
+        type: 'input',
+        name: 'genDesc',
+        message: "Provide a general description of the project:"
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: "What was your motivation to complete this project?",
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: "What problem does it solve?",
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: "What did you learn?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+    },
+    {
+        type: 'input',
+        name: 'gitUser',
+        message: "What is your GitHub username?",
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "Provide some installation instructions for users:",
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Enter usage instructions:",
+    },
+    {
+        type: 'input',
+        name: 'contrib',
+        message: "Provide some contribution instructions:",
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: "Provide some testing instructions:",
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: "Which license does this project operate under? (check one)",
+        choices: [
+            {
+                name: 'Apache license 2.0',
+            },
+            {
+                name: 'Creative Commons license family',
+            },
+            {
+                name: 'Do What The F*ck You Want To Public License',
+            },
+            {
+                name: 'MIT',
+            },
+            {
+                name: 'GNU General Public License v3.0',
+            },
+            {
+                name: 'PostgreSQL License',
+            },
+            {
+                name: 'none',
+            },
+        ],
+        validate(answer) {
+            if (answer.length < 1) {
+                return 'You must select a license, or choose "none".';
+            }
+            return true;
+        },
+    },
+    {
+        type: 'checkbox',
+        name: 'techTools',
+        message: "Which, if any, of these technologies does your project utilize?",
+        choices: [
+            {
+                name: 'HTML',
+            },
+            {
+                name: 'CSS',
+            },
+            {
+                name: 'Javascript',
+            },
+            {
+                name: 'Node.JS',
+            },
+            {
+                name: 'Python',
+            },
+            {
+                name: 'PHP',
+            },
+            {
+                name: 'none',
+            },
+        ],
+        validate(answer) {
+            if (answer.length < 1) {
+                return 'You must select at least one technology, or choose "none".';
+            }
+            return true;
+        },
+    }
+
+
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+inquirer.prompt(questions).then((answers) => {
 
-// TODO: Create a function to initialize app
-function init() { }
+    console.log(JSON.stringify(answers, null, '  '));
 
-// Function call to initialize app
-init();
+    // TODO: Create a function to write README file
+    //const title = (answers.title);
+    const motivation = (answers.motivation);
+
+    fs.writeFile('README.md',
+
+        // 'Hello Content', 
+
+        `## Title ${(answers.title)}    
+
+## Description - ${answers.genDesc}
+    
+- Motivation: ${answers.motivation}
+- Problem solved: ${answers.problem} 
+- Lessons learned: ${answers.learn}
+    
+## Table of Contents    
+   
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Credits](#credits)
+
+- [License](#license)
+
+- [Screenshot](#screenshot)
+    
+## Installation    
+
+- Steps required to install project:
+${answers.install}
+    
+## Usage
+
+- Here are usage instructions:
+    ${answers.usage}
+    
+## Screenshot
+
+![screenshot of app](/assets/images/screenshot.png)
+            
+    
+## Credits / Contact Info 
+
+- email me: <${answers.email}>
+- Find me on GitHub at <https://github.com/${answers.gitUser}>
+
+    
+    
+## License Info
+
+${answers.license}
+
+## Badges 🏆
+
+${answers.techTools}   
+    
+## How to Contribute
+    
+-If you'd like to become a contributor, here's how you can help:
+${answers.contrib}
+    
+## Tests
+  
+- How to run tests of the application:
+${answers.testing}    
+`,
+
+
+        function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+
+
+        });
+})
+
+
+
+// // TODO: Create a function to write README file
+// fs.writeFile(README.md, 'Hello Content', function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+
+
+
+// });
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt([questions])
+//         .then((answers) => {
+//             console.log(JSON.stringify(answers, null, ' '));
+//         });
+// }
+
+// // Function call to initialize app
+// init()
